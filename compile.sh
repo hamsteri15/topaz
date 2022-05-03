a82f0930b240f3603afc9a37c853c0d4add7bf9d
Petteri Peltonen <petteri.peltonen@vtt.fi>
2022-05-03 14:11:41 +0300

    rename src -> include

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 
 
 #tests
-g++ -Isrc/ -Itest/ test/test.cpp -o test_cpu.bin
-nvcc -Isrc/ -Itest/ -x cu test/test.cpp -o test_gpu.bin
+g++ -Iinclude/ -Itest/ test/test.cpp -o test_cpu.bin
+nvcc -Iinclude/ -Itest/ -x cu test/test.cpp -o test_gpu.bin
 
 #benchmarks
-#g++ -Isrc/ -Itest/ test/benchmark.cpp -o benchmark_cpu.bin
-#nvcc -Isrc/ -Itest/ -x cu test/benchmark.cpp -o benchmark_gpu.bin
+#g++ -Iinclude/ -Itest/ test/benchmark.cpp -o benchmark_cpu.bin
+#nvcc -Iinclude/ -Itest/ -x cu test/benchmark.cpp -o benchmark_gpu.bin
