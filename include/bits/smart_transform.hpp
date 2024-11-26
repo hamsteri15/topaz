cbc107738d11c9f8906ad0343b3ed3667a2f3600
Petteri Peltonen <petteri_peltonen@hotmail.com>
2024-11-26 16:54:59 +0200

    formatting

--- include/bits/smart_transform.hpp
+++ include/bits/smart_transform.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "traits.hpp"
-#include "range.hpp"
 #include "constant_range.hpp"
-namespace topaz{
-
+#include "range.hpp"
+#include "traits.hpp"
+namespace topaz {
 
 template <class T1,
           class T2,
@@ -13,19 +12,13 @@ template <class T1,
 inline CUDA_HOSTDEV auto
 smart_transform(const T1& lhs, const T2& rhs, BinaryOp f) {
 
-    if constexpr (BothRangesOrNumericArrays_v<T1, T2>)
-    {
+    if constexpr (BothRangesOrNumericArrays_v<T1, T2>) {
         return transform(lhs, rhs, f);
-    }
-    else if constexpr (IsRangeOrNumericArray_v<T1>)
-    {
+    } else if constexpr (IsRangeOrNumericArray_v<T1>) {
         return transform(lhs, make_constant_range(rhs, adl_size(lhs)), f);
-    }
-    else {
+    } else {
         return transform(make_constant_range(lhs, adl_size(rhs)), rhs, f);
     }
-
 }
 
-
-}
+} // namespace topaz
